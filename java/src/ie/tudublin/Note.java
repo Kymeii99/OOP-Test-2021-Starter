872945e69c9d971d03715475faf92fa442acb1bd
Kim Thai <kimthai@Kims-MacBook-Pro.local>
2021-04-21 17:05:31 +0100

    Adding Print and Load Methods

--- java/src/ie/tudublin/Note.java
+++ java/src/ie/tudublin/Note.java
@@ -1,4 +1,6 @@
-package ie;
+package ie.tudublin;
+
+import processing.data.TableRow;
 
 public class Note {
     private char note;
@@ -28,5 +30,8 @@ public class Note {
     public Note(char note, int duration) {
         this.note = note;
         this.duration = duration;
+    }
+
+    public Note(int i) {
     }
 }
--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -4,13 +4,34 @@ import java.util.ArrayList;
 
 
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
 
 public class ScoreDisplay extends PApplet
 {
+	ArrayList<Note> note = new ArrayList<Note>();
 	String score = "DEFGABcd";
 	//String score = "D2E2F2G2A2B2c2d2";
 	//String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
 	
+	int size = score.length();
+
+	public void printScore()
+    {
+        for(Note n: note)
+        {
+            println(n);
+        }
+    }
+
+	public void loadScore()
+	{
+		for (int i = 0; i <= score.length(); i++)
+		{
+			note[i] = score.charAt(i);
+		}
+	}
+
 	public void settings()
 	{
 		size(1000, 500);
@@ -23,12 +44,26 @@ public class ScoreDisplay extends PApplet
 
 	public void setup() 
 	{
-		size(1000,800);
+		loadScore();
+		printScore();
+	}
+
+	public void drawLines()
+	{
+		float xborder = width * 0.15f;
+
+		for ( int i = 0; i < 5 ; i++)
+		{
+			float x = map(i, -5, 5, xborder, height - xborder - 40);
+			stroke(0);
+			line(xborder, x, width - xborder, x);
+		}
 	}
 
 	public void draw()
 	{
 		background(255);
+		drawLines();
 		
 	}
 
