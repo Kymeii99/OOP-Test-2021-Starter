cbeeed2f62de465c1681caa332be6263ae290bcf
Kim Thai <kimthai@Kims-MacBook-Pro.local>
2021-04-21 20:48:18 +0100

    Drawing Notes

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -1,18 +1,22 @@
 package ie.tudublin;
 
 import java.util.ArrayList;
+
+import javax.swing.border.Border;
+
 import processing.core.PApplet;
 
 
 public class ScoreDisplay extends PApplet
 {
 	ArrayList<Note> note = new ArrayList<Note>();
-	//String score = "DEFGABcd";
-	String score = "D2E2F2G2A2B2c2d2";
+	String score = "DEFGABcd";
+	//String score = "D2E2F2G2A2B2c2d2";
 	//String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
-	
-	int size = score.length();
 
+	int size = score.length();
+	float border;
+	float borderHeight;
 	public void printScore()
     {
         for(Note n: note)
@@ -35,7 +39,7 @@ public class ScoreDisplay extends PApplet
 		{
 			char cNote = score.charAt(i);
 			int currDur;
-			
+
 			if(Character.isLetter(cNote))
 			{
 				if(i < size - 1 && Character.isDigit(score.charAt(i+1)))
@@ -56,6 +60,8 @@ public class ScoreDisplay extends PApplet
 	public void settings()
 	{
 		size(1000, 500);
+		border = width * 0.1f;
+		borderHeight = height * 0.1f;
 		// How to convert a character to a number
 		char c = '7'; // c holds the character 7 (55)
 		int i = c - '0'; // i holds the number 7 (55 - 48) 
@@ -64,59 +70,52 @@ public class ScoreDisplay extends PApplet
 
 	public void setup() 
 	{
+
 		loadScore();
 		printScore();
 	}
 
 	public void drawLines()
 	{
-		float xborder = width * 0.15f;
-
 		for ( int i = 0; i < 5 ; i++)
 		{
-			float x = map(i, -5, 5, xborder, height - xborder - 40);
+			float x = map(i, -5, 5, border, height - border - 40);
 			stroke(0);
-			line(xborder, x, width - xborder, x);
-		}
-	}
-
-	public void drawScore()
-	{
-		float border = width * 0.15f;
-		for ( int i = 0; i < size;i++)
-		{
-			float x = map(i, -3, 3, border, height - border - 40);
-			fill(0);
-			text(score.substring(i, i+1),x, border);
+			line(border, x, width - border, x);
 		}
 	}
 
 	public void draw()
 	{
 		background(255);
+		fill(0);
 		drawLines();
-		drawScore();
 		drawNotes();
-		
 	}
 
 	void drawNotes(){
-		float border = width * 0.15f;
-		float halfHeight = height / 2;
-		int position = 0;
-
-		for ( int i = 0; i < size;i++)
+		for ( int i = 0 ; i < note.size(); i++)
 		{
-			//float x = map(i, -5, 5, xborder, height - xborder - 40);
-			float y = map(i, 0, 2, border, height/2);
-			fill(0);
-			line(y + 10, halfHeight - 40,y + 10, halfHeight);
-			ellipse(y, height /2  , 20, 20);
+			Note n = note.get(i);
+			int note1,note2;
+			char music = n.getNote();
+
+			note1 = 'D';
+			note2 = 'a';
+
+			if (note1 > music)
+			{
+				music += 7;
+			}
+			else if(note2 < music)
+			{
+				music -=25;
+			}
+			float x = map(i, 0,note.size()-1, border + 10, width - border - 10);
+			float y = map(music, note1 - 4, note2 - 25, height/2 + borderHeight, height/2 - borderHeight );
+			ellipse(x, y + ((height/2) * 0.38f), 25, 25);
+			text(n.getNote(), x , 100);
+			line(x + 10, y + ((height/2) * 0.38f) , x + 10, y + ((height/2) * 0.38f)  - 40);
 		}
-	}
-
-	void mouseHover()
-	{
-
 	}
 }
